5039e7cf56395aaa168e06420d2ca21ac74e579d
Shubham Deshmukh <shubhamdeshm37@gmail.com>
2023-11-08 18:14:50 +0530

    Added some content in News component

--- newsapp/src/NewsItems.js
+++ newsapp/src/NewsItems.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 
 export class NewsItems extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description,imageUrl} = this.props;
     return (
-      <>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
           <img
-            src="https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.23.3/images/bbc-sport-logo.png"
+            src={imageUrl}
             className="card-img-top"
             alt="..."
           />
@@ -15,11 +15,11 @@ export class NewsItems extends Component {
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
             <a href="/newsItem" className="btn btn-primary">
-              Go somewhere
+              Read More
             </a>
           </div>
         </div>
-      </>
+      </div>
     );
   }
 }
